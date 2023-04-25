f777da8aaa2fde4ebb8a655d11a05e342e4b9774
Sawako Goshima <sawako@192-168-1-103.tpgi.com.au>
2023-04-25 11:41:23 +1000

    Made notes.js at routes folder and coded saving at db.json

---
+++ Develop/routes/notes.js
@@ -0,0 +1,54 @@
+const notes = require('express').Router();
+const fs = require('fs');
+
+// GET Route for retrieving all the notes
+// notes.get('/', (req, res) =>
+//     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+// );
+
+
+notes.post('/', (req, res) => {
+
+    const { title, text } = req.body;
+
+    if (title && text) {
+
+        const newNotes = {
+            title,
+            text,
+            // note_id: uuid(),
+        };
+
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                const parsedNotes = JSON.parse(data);
+                parsedNotes.push(newNotes);
+
+                fs.writeFile(
+                    './db/db.json',
+                    JSON.stringify(parsedNotes, null, 2),
+                    (writeErr) =>
+                        writeErr
+                            ? console.error(writeErr)
+                            : console.info('Successfully updated notes!')
+                );
+            }
+        });
+
+
+        const response = {
+            status: 'success',
+            body: newNotes,
+        };
+
+        console.log(response);
+
+        res.json(response);
+    } else {
+        res.json('Error in posting note');
+    }
+});
+
+module.exports = notes;
